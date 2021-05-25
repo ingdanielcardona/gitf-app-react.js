cc10f37d0f9eed81fb6d4d8168ffd4d9d4e36f59
ingdanielcardona <ingdanielcardona@hotmail.com>
2021-05-24 22:18:20 -0500

    First version

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,28 @@
+// @flow
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+
+    // const categories = ['One Punch, Dragon ball, Samurai X'];
+    const [categories, setCategories] = useState(['One Punch']);
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories} />
+            <hr />
+            <ol>
+                {
+                    categories.map(category =>
+                        <GifGrid 
+                            key = {category}
+                            category={category} />
+                    )
+                }
+            </ol>
+        </>
+    );
+};
+
